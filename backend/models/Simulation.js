20e3186e89a3b432f6e971ce2c9a4a759cc0505f
khushichhari <130910904+khushichhari@users.noreply.github.com>
2025-06-04 15:11:25 +0530

    Update Simulation.js

--- backend/models/Simulation.js
+++ backend/models/Simulation.js
@@ -1,12 +1,26 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
+
+// const simulationSchema = new mongoose.Schema({
+//   name: String,
+//   careerPath: String,
+//   result: Object,
+//   date: { type: Date, default: Date.now },
+// });
+
+// const Simulation = mongoose.model('Simulation', simulationSchema);
+
+// export default Simulation;
+
+
+import mongoose from "mongoose";
 
 const simulationSchema = new mongoose.Schema({
-  name: String,
-  careerPath: String,
-  result: Object,
+  name: { type: String, required: true },
+  careerPath: { type: String, required: true },
+  result: { type: Object, required: true },
   date: { type: Date, default: Date.now },
 });
 
-const Simulation = mongoose.model('Simulation', simulationSchema);
+const Simulation = mongoose.model("Simulation", simulationSchema);
 
 export default Simulation;
